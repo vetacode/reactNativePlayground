a26744bbd8d0b213320e65bf92e4d4bb0b130479
vetacode <vetacode@gmail.com>
2025-10-23 20:33:58 +0700

    menuItems challenges

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
-import WelcomeScreen from './WelcomeScreen';
+// import WelcomeScreen from './WelcomeScreen';
+import MenuItems from './components/MenuItems';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <WelcomeScreen />
+        {/* <WelcomeScreen /> */}
+        <MenuItems />
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
--- components/MenuItems.js
+++ components/MenuItems.js
@@ -1,12 +1,38 @@
-import * as React from 'react';
+import React from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
 
-export default function () {
-<View>
-    <FlatList>
-        
+const green = '#495E57';
+const yellow = '#F4CE14';
+const salmon = '#EE9972';
+const black = '#333333';
+const white = '#EDEFEE';
+
+export default function MenuItems () {
+<View style={menuStyles.container}> 
+     <Text style={menuStyles.headerText}>Menu Items</Text>
+    <FlatList style={menuStyles.innerContainer}>
+       
     </FlatList>
 </View>
 
 
 }
+
+
+
+const menuStyles = StyleSheet.create({
+    container: {
+        flex: 0.8,
+        // backgroundColor: green,
+    },
+
+    innerContainer: {
+        backgroundColor: salmon,
+    },
+    headerText: {
+        fontSize: 30,
+        flexWrap: 'wrap',
+        color: yellow,
+        
+    }
+}) 

6044689a884db8fd39d82ef5410dd44563bb190a
vetacode <vetacode@gmail.com>
2025-10-23 20:50:09 +0700

    assignment done

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -7,30 +7,82 @@ const salmon = '#EE9972';
 const black = '#333333';
 const white = '#EDEFEE';
 
+const menuItemsToDisplay = [
+  { name: 'Hummus', price: '$5.00', id: '1A' },
+  { name: 'Moutabal', price: '$5.00', id: '2B' },
+  { name: 'Falafel', price: '$7.50', id: '3C' },
+  { name: 'Marinated Olives', price: '$5.00', id: '4D' },
+  { name: 'Kofta', price: '$5.00', id: '5E' },
+  { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
+  { name: 'Lentil Burger', price: '$10.00', id: '7G' },
+  { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
+  { name: 'Kofta Burger', price: '$11.00', id: '9I' },
+  { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
+  { name: 'Fries', price: '$3.00', id: '11K' },
+  { name: 'Buttered Rice', price: '$3.00', id: '12L' },
+  { name: 'Bread Sticks', price: '$3.00', id: '13M' },
+  { name: 'Pita Pocket', price: '$3.00', id: '14N' },
+  { name: 'Lentil Soup', price: '$3.75', id: '15O' },
+  { name: 'Greek Salad', price: '$6.00', id: '16Q' },
+  { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
+  { name: 'Baklava', price: '$3.00', id: '18S' },
+  { name: 'Tartufo', price: '$3.00', id: '19T' },
+  { name: 'Tiramisu', price: '$5.00', id: '20U' },
+  { name: 'Panna Cotta', price: '$5.00', id: '21V' },
+];
+
+
+const Item = ({name, price}) => (
+
+    <View style={menuStyles.innerContainer}>
+        <Text style={menuStyles.itemText}>{name}</Text>
+        <Text style={menuStyles.itemText}>{price}</Text>
+
+    </View>
+
+)
+
 export default function MenuItems () {
+const renderItem = ({item}) =>
+<Item name={item.name} price={item.price}/>
+
+return (
+
 <View style={menuStyles.container}> 
      <Text style={menuStyles.headerText}>Menu Items</Text>
-    <FlatList style={menuStyles.innerContainer}>
-       
+    <FlatList data={menuItemsToDisplay} keyExtractor={(item)=> item.id} renderItem={renderItem}>
     </FlatList>
 </View>
 
-
+)
 }
 
 
 
 const menuStyles = StyleSheet.create({
     container: {
-        flex: 0.8,
+        flex: 1,
         // backgroundColor: green,
     },
 
     innerContainer: {
-        backgroundColor: salmon,
+        // backgroundColor: salmon,
+        padding: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
+
     headerText: {
         fontSize: 30,
+        color: white,
+        textAlign: 'center',
+        // marginTop: 10,
+        padding: 10,
+        backgroundColor: green,
+    },
+
+    itemText: {
+        fontSize: 20,
         flexWrap: 'wrap',
         color: yellow,
         

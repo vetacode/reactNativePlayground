770add4b038075a5b7e005fd645625e6314c8eca
vetacode <vetacode@gmail.com>
2025-10-24 15:06:57 +0700

    assignment done

--- components/SectionListAssign.js
+++ components/SectionListAssign.js
@@ -1,29 +1,48 @@
-import React from 'react';
-
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { SectionList, StyleSheet, Text, View } from 'react-native';
+import { salmon, white, yellow } from './MenuItems';
 
 const menuItemsToDisplay = [
-  { name: 'Hummus', price: '$5.00', id: '1A' },
-  { name: 'Moutabal', price: '$5.00', id: '2B' },
-  { name: 'Falafel', price: '$7.50', id: '3C' },
-  { name: 'Marinated Olives', price: '$5.00', id: '4D' },
-  { name: 'Kofta', price: '$5.00', id: '5E' },
-  { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
-  { name: 'Lentil Burger', price: '$10.00', id: '7G' },
-  { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
-  { name: 'Kofta Burger', price: '$11.00', id: '9I' },
-  { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
-  { name: 'Fries', price: '$3.00', id: '11K' },
-  { name: 'Buttered Rice', price: '$3.00', id: '12L' },
-  { name: 'Bread Sticks', price: '$3.00', id: '13M' },
-  { name: 'Pita Pocket', price: '$3.00', id: '14N' },
-  { name: 'Lentil Soup', price: '$3.75', id: '15O' },
-  { name: 'Greek Salad', price: '$6.00', id: '16Q' },
-  { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
-  { name: 'Baklava', price: '$3.00', id: '18S' },
-  { name: 'Tartufo', price: '$3.00', id: '19T' },
-  { name: 'Tiramisu', price: '$5.00', id: '20U' },
-  { name: 'Panna Cotta', price: '$5.00', id: '21V' },
+  {
+    title: 'Appetizers',
+    data: [
+      { name: 'Hummus', price: '$5.00' },
+      { name: 'Moutabal', price: '$5.00' },
+      { name: 'Falafel', price: '$7.50' },
+      { name: 'Marinated Olives', price: '$5.00' },
+      { name: 'Kofta', price: '$5.00' },
+      { name: 'Eggplant Salad', price: '$8.50' },
+    ],
+  },
+  {
+    title: 'Main Dishes',
+    data: [
+      { name: 'Lentil Burger', price: '$10.00' },
+      { name: 'Smoked Salmon', price: '$14.00' },
+      { name: 'Kofta Burger', price: '$11.00' },
+      { name: 'Turkish Kebab', price: '$15.50' },
+    ],
+  },
+  {
+    title: 'Sides',
+    data: [
+      { name: 'Fries', price: '$3.00', id: '11K' },
+      { name: 'Buttered Rice', price: '$3.00' },
+      { name: 'Bread Sticks', price: '$3.00' },
+      { name: 'Pita Pocket', price: '$3.00' },
+      { name: 'Lentil Soup', price: '$3.75' },
+      { name: 'Greek Salad', price: '$6.00' },
+      { name: 'Rice Pilaf', price: '$4.00' },
+    ],
+  },
+  {
+    title: 'Desserts',
+    data: [
+      { name: 'Baklava', price: '$3.00' },
+      { name: 'Tartufo', price: '$3.00' },
+      { name: 'Tiramisu', price: '$5.00' },
+      { name: 'Panna Cotta', price: '$5.00' },
+    ],
+  },
 ];
 
 const Item = ({ name, price }) => (
@@ -33,15 +52,23 @@ const Item = ({ name, price }) => (
   </View>
 );
 
+const separator = () => <View style={menuStyles.separator} />;
+
 const MenuItems = () => {
   const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
+  const renderSectionHeader = ({ section: { title } }) => (
+    <Text style={menuStyles.sectionHeader}>{title}</Text>
+  );
 
   return (
     <View style={menuStyles.container}>
-      <FlatList
-        data={menuItemsToDisplay}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}></FlatList>
+      <SectionList
+        sections={menuItemsToDisplay}
+        keyExtractor={(item, index) => item + index}
+        renderItem={renderItem}
+        renderSectionHeader={renderSectionHeader}
+        ItemSeparatorComponent={separator}
+      ></SectionList>
     </View>
   );
 };
@@ -56,9 +83,22 @@ const menuStyles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
+
+  sectionHeader: {
+    backgroundColor: salmon,
+    fontSize: 25,
+    textAlign: 'center',
+    padding: 5,
+  },
+
   itemText: {
-    color: '#F4CE14',
+    color: white,
     fontSize: 20,
+  },
+
+  separator: {
+    borderColor: yellow,
+    borderBottomWidth: 1,
   },
 });
 

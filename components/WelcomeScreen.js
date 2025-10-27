7ad8724652fe7e545712b1198e15baceb42350d8
vetacode <vetacode@gmail.com>
2025-10-27 13:23:29 +0700

    adding KeyboardAvoidingView wrapping ScrollView component

--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,30 +1,51 @@
 import { useState } from 'react';
-import { ScrollView, StyleSheet, Text, TextInput } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from 'react-native';
 
 export default function WelcomeScreen() {
   const [firstName, onChangeFirstName] = useState('');
+  const [lastName, onChangeLastName] = useState('');
+  const [message, onChangeMessage] = useState('');
   return (
-    <ScrollView style={styles.container} keyboardDismissMode='on-drag'>
-      <Text style={styles.headerText}>Welcome to Little Lemon</Text>
-      <Text style={styles.regularText}>
-        Little Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would love
-        to hear more about your experience with us!
-      </Text>
-      <TextInput
-        style={styles.inputBox}
-        value={firstName}
-        onChangeText={onChangeFirstName}
-        placeholder={'First Name'}
-      />
-    </ScrollView>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behaviour={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <ScrollView keyboardDismissMode='on-drag'>
+        <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+        <Text style={styles.regularText}>
+          Little Lemon is a charming neighborhood bistro that serves simple food
+          and classic cocktails in a lively but casual environment. We would
+          love to hear more about your experience with us!
+        </Text>
+        <TextInput
+          style={styles.inputBox}
+          value={firstName}
+          onChangeText={onChangeFirstName}
+          placeholder={'First Name'}
+        />
+        <TextInput
+          style={styles.inputBox}
+          value={lastName}
+          onChangeText={onChangeLastName}
+          placeholder={'Last Name'}
+        />
+        <TextInput
+          style={styles.inputBoxMessage}
+          value={message}
+          onChangeText={onChangeMessage}
+          placeholder={'Input Your Message'}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
-
-// const KeyBoardAvoidingView = ({behaviour, style}) => {
-// behaviour = {Platform.OS==="ios" ? "padding" : "position"}
-// style = {styes.container}
-// }
 
 const styles = StyleSheet.create({
   container: {
@@ -45,6 +66,15 @@ const styles = StyleSheet.create({
   },
   inputBox: {
     height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: 'EDEFEE',
+    backgroundColor: '#EDEFEE',
+  },
+  inputBoxMessage: {
+    height: 150,
     margin: 12,
     borderWidth: 1,
     padding: 10,

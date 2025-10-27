7c31d4df0f5f3a200b46efd4560046f0d10e23b8
vetacode <vetacode@gmail.com>
2025-10-27 12:48:09 +0700

    run in expo WelcomeScreen

--- App.js
+++ App.js
@@ -3,24 +3,24 @@ import { StyleSheet, View } from 'react-native';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import { black, green } from './components/MenuItems';
-// import WelcomeScreen from './WelcomeScreen';
+import WelcomeScreen from './WelcomeScreen';
 // import MenuItemsSectionList from './components/MenuItemsSectionList';
 // import SectionListAssign from './components/SectionListAssign';ß
 // import SectionListAssignAnswer from './components/SectionListAssignAnswer';
 // import MenuItems from './components/SectionListAssign';
-import LatihanSectionList from './components/LatihanSectionList';
+// import LatihanSectionList from './components/LatihanSectionList';
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-        {/* <WelcomeScreen /> */}
+        <WelcomeScreen />
         {/* <MenuItems /> */}
         {/* <SectionListAssign /> */}
         {/* <SectionListAssignAnswer /> */}
         {/* <MenuItemsSectionList /> */}
-        <LatihanSectionList />
+        {/* <LatihanSectionList /> */}
       </View>
       <View style={styles.footerContainer}>
         <LittleLemonFooter />
